29f206e33e5a06761f09d03c9a7d8af037a59bdc
stavkin11 <stavkin11@gmail.com>
2022-11-12 05:35:18 -0500

    displaying locations

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,20 +3,47 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello() {
-		return "hello world";
-	}
+	@GetMapping("/api")
+	public String printlocation() throws IOException {
+		URL obj = new URL("https://umassdining.com/uapp/get_infov2");
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			return response.toString();
+		} else {
+			return "GET did not work";
+		}
+	}
 }
 
