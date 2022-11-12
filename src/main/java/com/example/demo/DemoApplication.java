48a5b20e933ff7cf1152e3763e90dfed92cce725
stavkin11 <stavkin11@gmail.com>
2022-11-12 07:33:13 -0500

    json

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,16 +35,9 @@ public class DemoApplication {
 		if (responseCode == HttpURLConnection.HTTP_OK) { // success
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					con.getInputStream()));
-			JSONObject jsonObject = new JSONObject();
-			JSONArray jsonArray = new JSONArray();
-			String result = "";
-			String output = null;
-			while ((result = in.readLine()) != null) {
-				output = result.replace("[", "").replace("]", "");
-				jsonObject = new JSONObject(output);
-				jsonArray = new JSONArray(output);
-			}
-			return jsonObject;
+			JSONTokener tokener = new JSONTokener(in);
+			JSONObject json = new JSONObject(tokener);
+			return json;
 		}
 		else {
 			return new JSONObject();
