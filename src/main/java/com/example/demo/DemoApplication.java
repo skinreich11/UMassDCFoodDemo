435ec56b726b72d343e98b40d210d1b7233b5cfd
stavkin11 <stavkin11@gmail.com>
2022-11-12 07:04:20 -0500

    json

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,26 +10,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping
-	public String printlocation() throws IOException {
+	public String printlocations() throws IOException {
 		URL obj = new URL("https://umassdining.com/uapp/get_infov2");
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
 		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
 		if (responseCode == HttpURLConnection.HTTP_OK) { // success
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					con.getInputStream()));
@@ -44,6 +46,18 @@ public class DemoApplication {
 		} else {
 			return "GET did not work";
 		}
+	}
+	@GetMapping("/input")
+	public JSONObject printlocationoninput(@RequestParam String userInput) throws IOException {
+		URL obj = new URL("https://umassdining.com/uapp/get_infov2");
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+		}
+		return new JSONObject(con.getInputStream());
 	}
 }
 
