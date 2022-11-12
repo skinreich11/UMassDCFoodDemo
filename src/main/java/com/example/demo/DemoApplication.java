b1bad8ffcc9709a78700c009ab3ea73e79cb2203
stavkin11 <stavkin11@gmail.com>
2022-11-12 15:52:23 -0500

    backend works

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
+
+import java.util.regex.Pattern;
+import java.time.LocalDate;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.springframework.boot.SpringApplication;
@@ -11,13 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,43 +28,105 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	public List<Object> printlocations() throws IOException {
-		URL obj = new URL("https://umassdining.com/uapp/get_infov2");
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	@GetMapping("/testing")
+	public ArrayList<String> work() throws IOException {
+		LocalDate date = LocalDate.now();
+		ArrayList<String> result = new ArrayList<>();
+		URL link = new URL("https://umassdining.com/foodpro-menu-ajax?tid=1&date=" + date.getMonthValue() +
+				"%2F" + date.getDayOfMonth() + "%2F" + date.getYear());
+		HttpURLConnection con = (HttpURLConnection) link.openConnection();
 		con.setRequestMethod("GET");
 		int responseCode = con.getResponseCode();
 		if (responseCode == HttpURLConnection.HTTP_OK) { // success
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					con.getInputStream()));
 			JSONTokener tokener = new JSONTokener(in);
-			JSONArray json = new JSONArray(tokener);
-			return json.toList();
-		}
-		else {
-			return Collections.emptyList();
-		}
-	}
-	@GetMapping
-	public String getmenu(@RequestParam String food) throws IOException {
-		URL obj = new URL("https://umassdining.com/foodpro-menu-ajax?tid=0&date=11%2F12%2F2022");
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("GET");
-		int responseCode = con.getResponseCode();
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			JSONTokener tokener = new JSONTokener(in);
-			Map<String, Object> json = new JSONObject(tokener).toMap();
-			StringBuilder keys = new StringBuilder();
-			for(String meal : json.keySet()) {
-				keys.append(json.get(meal).toString());
+			JSONObject json = new JSONObject(tokener);
+			Iterator<String> keys = json.keys();
+			while (keys.hasNext()) {
+				String key = keys.next();
+				if (json.get(key) instanceof JSONObject) {
+					Iterator<String> keys2 = ((JSONObject) json.get(key)).keys();
+					while (keys2.hasNext()) {
+						String key2 = keys2.next();
+						if (((JSONObject) json.get(key)).get(key2) instanceof String) {
+							}
+						}
+					}
+				}
+			return result;
 			}
-			return keys.toString();
+		return null;
+	}
+
+	@GetMapping("/getfood")
+	public StringBuilder getmenu(@RequestParam String food) throws IOException {
+		LocalDate date = LocalDate.now();
+		ArrayList<String[]> allfoods = new ArrayList<>();
+		for (int i = 1; i < 5; i++) {
+			URL link = new URL("https://umassdining.com/foodpro-menu-ajax?tid=" + i + "&date=" + date.getMonthValue() +
+					"%2F" + date.getDayOfMonth() + "%2F" + date.getYear());
+			HttpURLConnection con = (HttpURLConnection) link.openConnection();
+			con.setRequestMethod("GET");
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) { // success
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				JSONTokener tokener = new JSONTokener(in);
+				JSONObject json = new JSONObject(tokener);
+				Iterator<String> keys = json.keys();
+				while (keys.hasNext()) {
+					String key = keys.next();
+					if (json.get(key) instanceof JSONObject) {
+						Iterator<String> keys2 = ((JSONObject) json.get(key)).keys();
+						while (keys2.hasNext()) {
+							String key2 = keys2.next();
+							if (((JSONObject) json.get(key)).get(key2) instanceof String) {
+								Pattern pattern = Pattern.compile("(?<=data-dish-name)(.*?)(?=\" )");
+								Matcher matcher = pattern.matcher((String) (((JSONObject) json.get(key)).get(key2)));
+								while (matcher.find()) {
+									if(matcher.group(1).substring(2).equalsIgnoreCase(food)){
+										switch (i) {
+											case 1:
+												allfoods.add(new String[]{"Worcester", key2, key});
+												break;
+											case 2:
+												allfoods.add(new String[]{"Franklin", key2, key});
+												break;
+											case 3:
+												allfoods.add(new String[]{"Hampshire", key2, key});
+												break;
+											default:
+												allfoods.add(new String[]{"Berkshire", key2, key});
+												break;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
-		return "Your amazing bot is sorry to report that " + food + "is not offered in any of the 4 main dining commons " +
-				"on campus :((((. However, in my greatness I have found an abundance of delicious food across the main " +
-				"dining commons. I implore you to search for more of your favorite foods, I am certain they are offered " +
-				"somewhere today!";
+		if (allfoods.size() != 0) {
+			StringBuilder answer = new StringBuilder("Oh Boy, Do I have good new for you! I have found your favorite food in at least one of the " +
+					"UMass award winning dining commons. The most delicious " + food + " can be found at ");
+			for (int i = 0; i < allfoods.size(); i++) {
+				if (i == allfoods.size() - 1 && i == 0) {
+					answer.append(allfoods.get(i)[0]).append(" in the ").append(allfoods.get(i)[1]).append(" section during ").append(allfoods.get(i)[2]).append(" time. What are you waiting for? Go before it runs out!");
+				}
+				else if (i == allfoods.size() - 1) {
+					answer.append("as well as at ").append(allfoods.get(i)[0]).append(" in the ").append(allfoods.get(i)[1]).append(" section during ").append(allfoods.get(i)[2]).append(" time. What are you waiting for? Go before it runs out!");
+				}
+				else {
+					answer.append(allfoods.get(i)[0]).append(" in the ").append(allfoods.get(i)[1]).append(" section during ").append(allfoods.get(i)[2]).append(" time, ");
+				}
+			}
+			return answer;
+		}
+		return new StringBuilder("I am so sorry to report that I have not found your favorite food today during any of the 4 main dining commons :(((. " +
+				"However, I am sure the UMass award winning dining commons have more amazing options for you to enjoy! Please search " +
+				"again for another another of your favorite food, I am sure UMass is serving it somewhere.");
 	}
 }
 
